c9dfa958ef0cb4023097f929ed58322c92b7e360
Muhammed Unais P <pmsons001@gmail.com>
2019-01-18 18:12:59 +0530

    backup : Fri Jan 18 18:12:58 IST 2019

--- gitpush.sh
+++ gitpush.sh
@@ -1,23 +1,33 @@
 now=$(date)
+date > last-push-time.txt
 
-git checkout other
-git add server.c
-git add server.h
-git add gitpush.sh
-git commit -m "Daily commit / other / ${now}"
-git push symflex-all other
+git add .
+git commit -m "backup : ${now}"
+git push provm-2019-synerg master
+git push provm-2019-github master
+
+
+
+
+
+#git checkout other
+#git add server.c
+#git add server.h
+#git add gitpush.sh
+#git commit -m "Daily commit / other / ${now}"
+#git push symflex-all other
 
 #
 
 
 # pushing linux
-cd kernel_source
-./gitpush-linux.sh "$now"
-cd ../
+#cd kernel_source
+#./gitpush-linux.sh "$now"
+#cd ../
 
 #pushing qemu
-cd qemu
-./gitpush-qemu.sh "$now"
+#cd qemu
+#./gitpush-qemu.sh "$now"
 
 
 #
